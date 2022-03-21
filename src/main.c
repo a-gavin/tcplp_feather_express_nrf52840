b80bfc2acd5acc8ce2126a1ad68bb4e9b4dc6ca0
Alex Gavin <a_gavin@icloud.com>
2022-03-21 13:25:49 -0700

    Implement OT TCPlp CLI for board

--- src/main.c
+++ src/main.c
@@ -5,10 +5,50 @@
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
-#include <sys/printk.h>
+#include <zephyr.h>
+#include <logging/log.h>
 
-int main() {
-    while (1) {
-        printk("Spinning\n");
-    }
+LOG_MODULE_REGISTER(cli_sample, CONFIG_OT_COMMAND_LINE_INTERFACE_LOG_LEVEL);
+
+#include <drivers/uart.h>
+#include <usb/usb_device.h>
+
+void main() {
+#if DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_shell_uart), zephyr_cdc_acm_uart)
+	int ret;
+	const struct device *dev;
+	uint32_t dtr = 0U;
+
+	ret = usb_enable(NULL);
+	if (ret != 0) {
+		LOG_ERR("Failed to enable USB");
+		return;
+	}
+
+	dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_shell_uart));
+	if (dev == NULL) {
+		LOG_ERR("Failed to find specific UART device");
+		return;
+	}
+
+	LOG_INF("Waiting for host to be ready to communicate");
+
+	/* Data Terminal Ready - check if host is ready to communicate */
+	while (!dtr) {
+		ret = uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
+		if (ret) {
+			LOG_ERR("Failed to get Data Terminal Ready line state: %d",
+				ret);
+			continue;
+		}
+		k_msleep(100);
+	}
+
+	/* Data Carrier Detect Modem - mark connection as established */
+	(void)uart_line_ctrl_set(dev, UART_LINE_CTRL_DCD, 1);
+	/* Data Set Ready - the NCP SoC is ready to communicate */
+	(void)uart_line_ctrl_set(dev, UART_LINE_CTRL_DSR, 1);
+#endif
+
+    LOG_INF("Spinning.\n");
 }
