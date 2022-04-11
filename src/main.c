14a5bbe587ab0eed2f6a8b97283c247e810bde10
Alex Gavin <alex.gavin@wsu.edu>
2022-04-11 12:42:05 -0700

    Begin impl of zephyr app. Successfully connects

--- src/main.c
+++ src/main.c
@@ -5,17 +5,44 @@
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
-#include <zephyr.h>
-#include <logging/log.h>
+#define APP_PORT	2424
+#define LISTEN_BACKLOG_NUM 1
 
+#include <logging/log.h>
 LOG_MODULE_REGISTER(tcp_app, LOG_LEVEL_DBG);
+
+#include <zephyr.h>
+#include <shell/shell.h>
+
+#include <net/socket.h>
+#include <net/net_ip.h>
 
 #include <drivers/uart.h>
 #include <usb/usb_device.h>
 
+// TODO: Application shell interface
+//SHELL_STATIC_SUBCMD_SET_CREATE(app_cmds,
+//	SHELL_CMD(connect, NULL,
+//		  "Connect to other benchmark device\n",
+//		  test),
+//	SHELL_SUBCMD_SET_END
+//);
+
+//SHELL_STATIC_SUBCMD_SET_CREATE(app_cmds,
+//	SHELL_CMD(start, NULL,
+//		  "Connect to other benchmark device\n",
+//		  start_benchmark),
+//	SHELL_SUBCMD_SET_END
+//);
+
+//SHELL_CMD_REGISTER(tcp_app, &app_cmds,
+//		   "Benchmark application commands", NULL);
+
 void main() {
-#if DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_shell_uart), zephyr_cdc_acm_uart)
 	int ret;
+
+#if DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_shell_uart), zephyr_cdc_acm_uart)
+	/* Setup UART-based shell */
 	const struct device *dev;
 	uint32_t dtr = 0U;
 
@@ -50,5 +77,51 @@ void main() {
 	(void)uart_line_ctrl_set(dev, UART_LINE_CTRL_DSR, 1);
 #endif
 
-    LOG_INF("Spinning.\n");
+#ifdef IS_LISTENER
+	/* Setup and listen on port APP_PORT for incoming TCP connections */
+	int sd, conn;
+	struct sockaddr_in6 host_addr;
+	socklen_t host_addr_len = sizeof(host_addr);
+
+	struct sockaddr_in conn_addr;
+	socklen_t conn_addr_len = sizeof(conn_addr);
+
+	(void)memset(&host_addr, 0, host_addr_len);
+	host_addr.sin6_family = AF_INET6;
+	host_addr.sin6_port = htons(APP_PORT);
+
+
+	sd = socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP);
+	if (sd < 0) {
+		LOG_ERR("Failed to create TCP socket (IPv6): %d", errno);
+		return;
+	}
+
+	ret = bind(sd, (struct sockaddr *) &host_addr, host_addr_len);
+	if (ret < 0) {
+		LOG_ERR("Failed to bind TCP socket (IPv6): %d", errno);
+		return;
+	}
+
+	ret = listen(sd, LISTEN_BACKLOG_NUM);
+	if (ret < 0) {
+		LOG_ERR("Failed to listen on TCP socket (IPv6): %d", errno);
+		ret = -errno;
+	}
+	LOG_INF("Waiting for TCP connection on port %d (IPv6)...", APP_PORT);
+
+
+	conn = accept(sd, (struct sockaddr *) &conn_addr, &conn_addr_len);
+	if (conn < 0) {
+		LOG_ERR("IPv6 accept error (%d)", -errno);
+		return;
+	}
+	LOG_INF("TCP (IPv6)): Accepted connection");
+
+	LOG_INF("Sleeping for 5 sec");
+	k_msleep(5000);
+
+	LOG_INF("Shutting down connection");
+	close(sd);
+#endif // IS_LISTENER
 }
