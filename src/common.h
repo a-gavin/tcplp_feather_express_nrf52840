2cd5809a1b6aa7313dca1d4a8ab8df5bd37c5181
Alex Gavin <alex.gavin@wsu.edu>
2022-04-13 01:25:19 -0700

    Impl simple send/recv transfer between connector/listener devices

---
+++ src/common.h
@@ -0,0 +1,65 @@
+/**
+ * @brief TCPlp application
+ * 
+ * Copyright (c) Alex Gavin, Brandon Lavinsky.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#include <zephyr.h>
+#include <shell/shell.h>
+
+#include <net/socket.h>
+#include <net/net_ip.h>
+
+#include <drivers/uart.h>
+#include <usb/usb_device.h>
+
+#define APP_PORT			2424
+//#define MAXBUFSIZE			2599
+#define MAXBUFSIZE			1000
+#define LISTEN_BACKLOG_NUM 	10
+
+/* Globals */
+int sd, conn_sd;
+
+#if IS_LISTENER == 1
+char recv_buf[MAXBUFSIZE+1];
+#else
+char send_buf[MAXBUFSIZE+1];
+#endif
+
+/* Prototypes */
+void cmd_init();
+void cmd_listen();
+void cmd_benchmark_recv();
+void cmd_connect();
+void cmd_benchmark_send();
+void cmd_quit();
+
+/* Application shell interface */
+SHELL_STATIC_SUBCMD_SET_CREATE(app_cmds,
+	SHELL_CMD(init, NULL,
+		  "Initialize session\n",
+		  cmd_init),
+#if IS_LISTENER == 1
+	SHELL_CMD(listen, NULL,
+		  "Listen for connections (blocking)\n",
+		  cmd_listen),
+	SHELL_CMD(benchmark_recv, NULL,
+		  "Receive data for benchmark\n",
+		  cmd_benchmark_recv),
+#else
+	SHELL_CMD(connect, NULL,
+		  "Connect to listening device\n",
+		  cmd_connect),
+	SHELL_CMD(benchmark_send, NULL,
+		  "Send data for benchmark\n",
+		  cmd_benchmark_send),
+#endif
+	SHELL_CMD(quit, NULL,
+		  "Quit session\n",
+		  cmd_quit),
+	SHELL_SUBCMD_SET_END
+);
+SHELL_CMD_REGISTER(tcp_app, &app_cmds,
+		   "Benchmark application commands", NULL);
--- src/main.c
+++ src/main.c
@@ -5,44 +5,154 @@
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
-#define APP_PORT	2424
-#define LISTEN_BACKLOG_NUM 1
-
 #include <logging/log.h>
 LOG_MODULE_REGISTER(tcp_app, LOG_LEVEL_DBG);
 
-#include <zephyr.h>
-#include <shell/shell.h>
+#include "common.h"
 
-#include <net/socket.h>
-#include <net/net_ip.h>
+void cmd_init() {
+	/* Setup socket for TCP conn_sdections */
+	sd = socket(AF_INET6, SOCK_STREAM, 0);
+	if (sd < 0) {
+		LOG_ERR("Failed to create TCP socket (IPv6): %d", -errno);
+		sd = 0;
+		return;
+	}
+	LOG_DBG("Initialized socket");
+}
 
-#include <drivers/uart.h>
-#include <usb/usb_device.h>
+#if IS_LISTENER == 1
+void cmd_listen() {
+	int ret;
+	struct sockaddr_in6 host_addr;
+	struct sockaddr_in6 conn_addr;
+	socklen_t host_addr_len = sizeof(host_addr);
+	socklen_t conn_addr_len = sizeof(conn_addr);
+	char addr_str[32];
 
-// TODO: Application shell interface
-//SHELL_STATIC_SUBCMD_SET_CREATE(app_cmds,
-//	SHELL_CMD(connect, NULL,
-//		  "Connect to other benchmark device\n",
-//		  test),
-//	SHELL_SUBCMD_SET_END
-//);
+	if (sd < 0) {
+		LOG_ERR("Must initialize session before running \"listen\"");
+		return;
+	}
 
-//SHELL_STATIC_SUBCMD_SET_CREATE(app_cmds,
-//	SHELL_CMD(start, NULL,
-//		  "Connect to other benchmark device\n",
-//		  start_benchmark),
-//	SHELL_SUBCMD_SET_END
-//);
+	/* Bind, listen, and accept incoming TCP connections (blocking) */
+	(void)memset(&host_addr, 0, host_addr_len);
+	host_addr.sin6_family = AF_INET6;
+	host_addr.sin6_port = htons(APP_PORT);
+	net_ipaddr_copy(&host_addr.sin6_addr, &in6addr_any);
 
-//SHELL_CMD_REGISTER(tcp_app, &app_cmds,
-//		   "Benchmark application commands", NULL);
+	ret = bind(sd, (struct sockaddr *) &host_addr, host_addr_len);
+	if (ret < 0) {
+		LOG_ERR("Failed to bind TCP socket (IPv6): %d", -errno);
+		return;
+	}
+
+	ret = listen(sd, LISTEN_BACKLOG_NUM);
+	if (ret < 0) {
+		LOG_ERR("Failed to listen on TCP socket (IPv6): %d", -errno);
+		ret = -errno;
+	}
+
+	LOG_DBG("Waiting for TCP connection on port %d (IPv6)...", APP_PORT);
+	conn_sd = accept(sd, (struct sockaddr *) &conn_addr, &conn_addr_len);
+	if (conn_sd < 0) {
+		LOG_ERR("IPv6 accept error (%d)", -errno);
+		return;
+	}
+
+	inet_ntop(conn_addr.sin6_family, &conn_addr.sin6_addr, addr_str, sizeof(addr_str));
+	LOG_DBG("TCP (IPv6): Accepted connection from %s", log_strdup(addr_str));
+}
+#else
+void cmd_connect() {
+	struct sockaddr_in6 conn_addr;
+	socklen_t conn_addr_len = sizeof(conn_addr);
+
+	if (sd < 0) {
+		LOG_ERR("Must initialize session before running \"listen\"");
+		return;
+	}
+
+	/* Connect to listening device */
+	(void)memset(&conn_addr, 0, conn_addr_len);
+	conn_addr.sin6_family = AF_INET6;
+	conn_addr.sin6_port = htons(APP_PORT);
+	if (inet_pton(AF_INET6, "2001:db8::1", &(conn_addr.sin6_addr)) != 1) {
+		LOG_ERR("Failed to translate address");
+	}
+
+	conn_sd = connect(sd, (struct sockaddr *) &conn_addr, conn_addr_len);
+	if (conn_sd < 0) {
+		LOG_ERR("Failed to connect: %d", -errno);
+	}
+	LOG_DBG("Successfully connected");
+}
+#endif
+
+#if IS_LISTENER == 1
+void cmd_benchmark_recv() {
+	if (sd < 0) {
+		LOG_ERR("Must initialize session before running \"bencmark_recv\"");
+		return;
+	} else if (conn_sd < 0) {
+		LOG_ERR("Must listen for connections before running \"bencmark_recv\"");
+		return;
+	}
+
+	ssize_t bytes_recvd = recv(conn_sd, recv_buf, sizeof(char)*MAXBUFSIZE, 0);
+	LOG_DBG("Num bytes recv: %d", bytes_recvd);
+	if (bytes_recvd < 0) {
+		LOG_ERR("Receive failed: %d", -errno);
+		return;
+	} else if (bytes_recvd != MAXBUFSIZE) {
+		LOG_ERR("Receive failed: received %d bytes, not %d bytes", bytes_recvd, MAXBUFSIZE);
+		return;
+	}
+	recv_buf[MAXBUFSIZE] = '\0';
+	LOG_DBG("Recv output: %s", log_strdup(recv_buf));
+}
+#else
+void cmd_benchmark_send() {
+	if (sd < 0) {
+		LOG_ERR("Must initialize session before running \"bencmark_recv\"");
+		return;
+	} else if (conn_sd < 0) {
+		LOG_ERR("Must listen for connections before running \"bencmark_recv\"");
+		return;
+	}
+
+	ssize_t bytes_sent = send(conn_sd, send_buf, sizeof(char)*MAXBUFSIZE, 0);
+	LOG_DBG("Num bytes sent: %d", bytes_sent);
+	if (bytes_sent < 0) {
+		LOG_ERR("Send failed: %d", -errno);
+		return;
+	} else if (bytes_sent != MAXBUFSIZE) {
+		LOG_ERR("Send failed: sent %d bytes, not %d bytes", bytes_sent, MAXBUFSIZE);
+		return;
+	}
+}
+#endif
+
+void cmd_quit() {
+	/* Tear down connection (if setup) and socket */
+	if (sd < 0) {
+		LOG_ERR("Must initialize session before running \"quit\"");
+		return;
+	}
+
+	if (conn_sd >= 0) {
+		LOG_DBG("Closing connection.");
+		close(conn_sd);
+	}
+
+	LOG_DBG("Closing socket.");
+	close(sd);
+}
 
 void main() {
-	int ret;
-
 #if DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_shell_uart), zephyr_cdc_acm_uart)
 	/* Setup UART-based shell */
+	int ret;
 	const struct device *dev;
 	uint32_t dtr = 0U;
 
@@ -58,14 +168,13 @@ void main() {
 		return;
 	}
 
-	LOG_INF("Waiting for host to be ready to communicate");
+	LOG_DBG("Waiting for host to be ready to communicate");
 
 	/* Data Terminal Ready - check if host is ready to communicate */
 	while (!dtr) {
 		ret = uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
 		if (ret) {
-			LOG_ERR("Failed to get Data Terminal Ready line state: %d",
-				ret);
+			LOG_ERR("Failed to get Data Terminal Ready line state: %d", ret);
 			continue;
 		}
 		k_msleep(100);
@@ -75,53 +184,15 @@ void main() {
 	(void)uart_line_ctrl_set(dev, UART_LINE_CTRL_DCD, 1);
 	/* Data Set Ready - the NCP SoC is ready to communicate */
 	(void)uart_line_ctrl_set(dev, UART_LINE_CTRL_DSR, 1);
+#else
+#error Application requires UART-based shell
 #endif
 
-#ifdef IS_LISTENER
-	/* Setup and listen on port APP_PORT for incoming TCP connections */
-	int sd, conn;
-	struct sockaddr_in6 host_addr;
-	socklen_t host_addr_len = sizeof(host_addr);
-
-	struct sockaddr_in conn_addr;
-	socklen_t conn_addr_len = sizeof(conn_addr);
-
-	(void)memset(&host_addr, 0, host_addr_len);
-	host_addr.sin6_family = AF_INET6;
-	host_addr.sin6_port = htons(APP_PORT);
-
-
-	sd = socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP);
-	if (sd < 0) {
-		LOG_ERR("Failed to create TCP socket (IPv6): %d", errno);
-		return;
-	}
-
-	ret = bind(sd, (struct sockaddr *) &host_addr, host_addr_len);
-	if (ret < 0) {
-		LOG_ERR("Failed to bind TCP socket (IPv6): %d", errno);
-		return;
-	}
-
-	ret = listen(sd, LISTEN_BACKLOG_NUM);
-	if (ret < 0) {
-		LOG_ERR("Failed to listen on TCP socket (IPv6): %d", errno);
-		ret = -errno;
-	}
-	LOG_INF("Waiting for TCP connection on port %d (IPv6)...", APP_PORT);
-
-
-	conn = accept(sd, (struct sockaddr *) &conn_addr, &conn_addr_len);
-	if (conn < 0) {
-		LOG_ERR("IPv6 accept error (%d)", -errno);
-		return;
-	}
-	LOG_INF("TCP (IPv6)): Accepted connection");
-
-	LOG_INF("Sleeping for 5 sec");
-	k_msleep(5000);
-
-	LOG_INF("Shutting down connection");
-	close(sd);
-#endif // IS_LISTENER
+#if IS_LISTENER == 1
+	LOG_DBG("Listener device running");
+	memset(&recv_buf, 'o', MAXBUFSIZE);
+#else
+	LOG_DBG("Connector device running");
+	memset(&send_buf, 'x', MAXBUFSIZE);
+#endif
 }
